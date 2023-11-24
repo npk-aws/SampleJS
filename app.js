fcde134ecdebc1be3b570759a557d1ba23890876
Karthik <karthiknp836@gmail.com>
2023-11-24 13:55:42 +0530

    Third

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Elastic Beanstalk!');
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// POST endpoint
+app.post('/api/hello', (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Hello, ${name || 'World'}!` });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
