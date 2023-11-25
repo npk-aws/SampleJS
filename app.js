2710407c5846c6ef6e4cb014e86736de6e3a29ee
Karthik <karthiknp836@gmail.com>
2023-11-25 16:05:11 +0530

    Thirdf1eq

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
+// File: app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// POST endpoint
 app.post('/api/hello', (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `Hello, ${name || 'World'}!` });
+  res.status(200).json({ message: 'Hello World!' });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
